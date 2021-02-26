11ca4ed1210baaa6a3dc2b6ef62350258f7c2ef4
takatakaakiaki <xiaozhangzuoteng903@gmail.com>
2021-02-26 16:04:53 +0900

    テーブル作成とアソシエーション、バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,18 @@
 class Item < ApplicationRecord
+  
+  belongs_to :user
+  has_one_attached :image
+  
+  with_options presence: true do
+    validates :item_name
+    validates :information
+    validates :category_id
+    validates :status_id
+    validates :shipping_fee_id
+    validates :prefecture_id
+    validates :schedule_id
+    validates :price
+  end
+
+
 end

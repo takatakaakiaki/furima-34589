604ade4fec34e5c308342d9fa233af822cd0459b
takatakaakiaki <xiaozhangzuoteng903@gmail.com>
2021-03-01 18:17:35 +0900

    商品詳細の表示　完成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
 
   def index
-    @items = Item.all.order("created_at DESC")
+    @items = Item.all.order('created_at DESC')
   end
 
   def new
@@ -24,8 +24,9 @@ class ItemsController < ApplicationController
   end
 
   private
-  def item_params
-    params.require(:item).permit(:item_name, :information, :category_id, :status_id, :shipping_fee_id, :prefecture_id, :schedule_id, :price, :image).merge(user_id: current_user.id)
-  end
 
+  def item_params
+    params.require(:item).permit(:item_name, :information, :category_id, :status_id, :shipping_fee_id, :prefecture_id,
+                                 :schedule_id, :price, :image).merge(user_id: current_user.id)
+  end
 end
--- app/models/prefecture.rb
+++ app/models/prefecture.rb
@@ -1,6 +1,6 @@
 class Prefecture < ActiveHash::Base
   self.data = [
-    { id: 1, name: '---' },    
+    { id: 1, name: '---' },
     { id: 2, name: '北海道' },
     { id: 3, name: '青森県' },
     { id: 4, name: '岩手県' },
@@ -27,7 +27,7 @@ class Prefecture < ActiveHash::Base
     { id: 25, name: '三重県' },
     { id: 26, name: '滋賀県' },
     { id: 27, name: '京都府' },
-    { id: 28, name: '大阪府' },   
+    { id: 28, name: '大阪府' },
     { id: 29, name: '兵庫県' },
     { id: 30, name: '奈良県' },
     { id: 31, name: '和歌山県' },
@@ -47,10 +47,9 @@ class Prefecture < ActiveHash::Base
     { id: 45, name: '大分県' },
     { id: 46, name: '宮崎県' },
     { id: 47, name: '鹿児島県' },
-    { id: 48, name: '沖縄県' },
+    { id: 48, name: '沖縄県' }
   ]
 
   include ActiveHash::Associations
   has_many :items
-
-  end
+end
